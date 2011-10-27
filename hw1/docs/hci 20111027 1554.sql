54d1062c657fa997c75da5366fe483e0d51476af
Yuhang Ge <abeyuhang@gmail.com>
2011-10-27 15:55:20 +0800

    hw1

--- hw1/docs/hci 20111027 1554.sql
+++ hw1/docs/hci 20111027 1554.sql
@@ -1,134 +1,167 @@
--- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
+-- MySQL Administrator dump 1.4
 --
--- Host: 127.0.0.1    Database: hci_hw1
 -- ------------------------------------------------------
--- Server version	5.1.50-community
+-- Server version	5.1.55-community
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 
 --
--- Current Database: `hci_hw1`
+-- Create schema hci_hw1
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `hci_hw1` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `hci_hw1`;
+CREATE DATABASE IF NOT EXISTS hci_hw1;
+USE hci_hw1;
 
 --
--- Table structure for table `class`
+-- Definition of table `class`
 --
 
 DROP TABLE IF EXISTS `class`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `class` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `tid` int(11) NOT NULL COMMENT '老师id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `class`
 --
 
-LOCK TABLES `class` WRITE;
 /*!40000 ALTER TABLE `class` DISABLE KEYS */;
-INSERT INTO `class` VALUES (1,'人机交互的软件工程方法',1),(2,'个性化二维码设计及编解码研究',2),(3,'软件构造',2),(4,' 手写文档识别研究',1);
+INSERT INTO `class` (`id`,`name`,`tid`) VALUES 
+ (1,'人机交互的软件工程方法',1),
+ (2,'个性化二维码设计及编解码研究',2),
+ (3,'软件构造',2),
+ (4,' 手写文档识别研究',1);
 /*!40000 ALTER TABLE `class` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
--- Table structure for table `record`
+-- Definition of table `record`
 --
 
 DROP TABLE IF EXISTS `record`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `record` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
   `type` smallint(6) NOT NULL COMMENT '类型：\n1：点名\n2：抽查',
   `cid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `record`
 --
 
-LOCK TABLES `record` WRITE;
 /*!40000 ALTER TABLE `record` DISABLE KEYS */;
-INSERT INTO `record` VALUES (1,'2011-10-18 14:11:00',1,1),(2,'2011-10-18 14:11:00',2,1);
+INSERT INTO `record` (`id`,`date`,`type`,`cid`) VALUES 
+ (1,'2011-10-18 14:11:00',1,1),
+ (2,'2011-10-18 14:11:00',2,1),
+ (3,'2011-10-25 00:00:00',1,4),
+ (4,'2011-10-25 00:00:00',1,1),
+ (5,'2011-10-27 00:00:00',1,1),
+ (6,'2011-10-27 00:00:00',1,4);
 /*!40000 ALTER TABLE `record` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
--- Table structure for table `stu_rec`
+-- Definition of table `stu_rec`
 --
 
 DROP TABLE IF EXISTS `stu_rec`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stu_rec` (
   `rid` int(11) NOT NULL,
   `sid` int(11) NOT NULL,
   `comment` varchar(255) DEFAULT NULL COMMENT '备注\n\n解释这个学生这次未到记录的原因，如生病请假'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `stu_rec`
 --
 
-LOCK TABLES `stu_rec` WRITE;
 /*!40000 ALTER TABLE `stu_rec` DISABLE KEYS */;
+INSERT INTO `stu_rec` (`rid`,`sid`,`comment`) VALUES 
+ (3,9,NULL),
+ (3,15,NULL),
+ (3,14,NULL),
+ (3,5,NULL),
+ (3,10,NULL),
+ (3,7,NULL),
+ (3,8,NULL),
+ (4,18,NULL),
+ (4,19,NULL),
+ (4,2,NULL),
+ (4,7,NULL),
+ (4,12,NULL),
+ (4,16,NULL),
+ (4,10,NULL),
+ (4,8,NULL),
+ (4,6,NULL),
+ (5,7,NULL),
+ (5,19,NULL),
+ (5,17,NULL),
+ (6,1,NULL),
+ (6,3,NULL),
+ (6,5,NULL),
+ (6,9,NULL);
 /*!40000 ALTER TABLE `stu_rec` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
--- Table structure for table `student`
+-- Definition of table `student`
 --
 
 DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `num` varchar(45) NOT NULL COMMENT '学号',
   `name` varchar(45) NOT NULL COMMENT '姓名',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `student`
 --
 
-LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (1,'081251041','葛羽航'),(2,'081251037','高运'),(3,'081251041','葛馨阳'),(4,'081251042','龚晨'),(5,'081251001','某某一'),(6,'081251002','某某二'),(7,'081251003','某某三'),(8,'081251004','某某四'),(9,'081251005','某某五'),(10,'081251006','某某六'),(11,'081251007','某某七'),(12,'081251008','某某八'),(13,'081251009','某某九'),(14,'081251010','某某十'),(15,'081251011','某某甲'),(16,'081251012','某某乙'),(17,'081251013','某某丙'),(18,'081251014','某某丁'),(19,'081251015','某某卯');
+INSERT INTO `student` (`id`,`num`,`name`) VALUES 
+ (1,'081251041','葛羽航'),
+ (2,'081251037','高运'),
+ (3,'081251041','葛馨阳'),
+ (4,'081251042','龚晨'),
+ (5,'081251001','某某一'),
+ (6,'081251002','某某二'),
+ (7,'081251003','某某三'),
+ (8,'081251004','某某四'),
+ (9,'081251005','某某五'),
+ (10,'081251006','某某六'),
+ (11,'081251007','某某七'),
+ (12,'081251008','某某八'),
+ (13,'081251009','某某九'),
+ (14,'081251010','某某十'),
+ (15,'081251011','某某甲'),
+ (16,'081251012','某某乙'),
+ (17,'081251013','某某丙'),
+ (18,'081251014','某某丁'),
+ (19,'081251015','某某卯');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
--- Table structure for table `teacher`
+-- Definition of table `teacher`
 --
 
 DROP TABLE IF EXISTS `teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teacher` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -136,18 +169,19 @@ CREATE TABLE `teacher` (
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `teacher`
 --
 
-LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES (1,'冯桂焕','fgy@xxx.com','123456'),(2,'郑涛','zt@xxx.com','123456');
+INSERT INTO `teacher` (`id`,`name`,`email`,`password`) VALUES 
+ (1,'冯桂焕','fgh@xxx.com','123456'),
+ (2,'郑涛','zt@xxx.com','123456');
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -155,6 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-10-18 14:17:34
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
